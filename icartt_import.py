d65d74d9c523f3cbbc154e2d16b194fead31103e
Daniel Hueholt <dmhuehol@ncsu.edu>
2020-11-13 14:01:47 -0700

    Back to the basics

--- icartt_import.py
+++ icartt_import.py
@@ -2,17 +2,17 @@ import PseudoNetCDF as pnc
 
 # Sphinx of the black quartz heed my vow
 
-## comemnt 
+## comemnt
 
 inpath = '/Users/dhueholt/Documents/ATS621/MER-TOGA_DC8_20171006_R19.ict'
 #outpath = '<path-for-output>'
 infile = pnc.pncopen(inpath, format = 'ffi1001')
-dir(infile)
+# dir(infile)
 # Print CDL representation - good for learning dimensions, variables, and properties
-# print(infile)
+print(infile)
 # Optionally, add dimension slicing
 # infile = infile.sliceDimensions(<layer-dim-name> = 0)
 # infile = infile.applyAlongDimenions(<time-dim-name> = 'mean')
-patches = infile.plot('<varkey>', plottype = 'longitude-latitude')
-patches.axes.figure.savefig('/Users/dhueholt/Documents/')
+# patches = infile.plot('<varkey>', plottype = 'longitude-latitude')
+# patches.axes.figure.savefig('/Users/dhueholt/Documents/')
 # infile.save(outpath)
