fa905e7562d570881915b516302f39f459092e11
Daniel Hueholt <dmhuehol@ncsu.edu>
2020-11-13 13:45:12 -0700

    Test push

--- icartt_import.py
+++ icartt_import.py
@@ -1,4 +1,7 @@
 import PseudoNetCDF as pnc
+
+# Sphinx of the black quartz heed my vow
+
 inpath = '/Users/dhueholt/Documents/ATS621/MER-TOGA_DC8_20171006_R19.ict'
 #outpath = '<path-for-output>'
 infile = pnc.pncopen(inpath, format = 'ffi1001')
@@ -8,6 +11,6 @@ dir(infile)
 # Optionally, add dimension slicing
 # infile = infile.sliceDimensions(<layer-dim-name> = 0)
 # infile = infile.applyAlongDimenions(<time-dim-name> = 'mean')
-# patches = infile.plot('<varkey>', plottype = 'longitude-latitude')
-# patches.axes.figure.savefig('<path-for-figure>')
+patches = infile.plot('<varkey>', plottype = 'longitude-latitude')
+patches.axes.figure.savefig('/Users/dhueholt/Documents/')
 # infile.save(outpath)
